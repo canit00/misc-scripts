7644bf450625bb505591ac8fadcf1c2a9e64b163
kspyy <krispey@gmail.com>
2023-07-16 14:00:32 -0700

    Create export.sh

---
+++ export.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# export and clean up ArgoCD apps and projects manifests to respective file
+# uses krew plugin: kubectl-neat to clean up manifest output https://github.com/itaysk/kubectl-neat
+# krew install https://krew.sigs.k8s.io/docs/user-guide/setup/install/
+# have only run this from a mac, also may need to make file executable (chmod +x export.sh) 
+
+apps_file="apps.yaml"
+projects_file="projects.yaml"
+namespace="argocd"
+
+apps() {
+    # Clear existing content in the apps file
+    > "$apps_file"
+
+    apps=$(kubectl get apps -n $namespace --no-headers -o custom-columns=":metadata.name")
+
+    for app in $apps; do
+        kubectl get apps $app -n $namespace -o yaml | kubectl neat >> "$apps_file"
+        echo "---" >> "$apps_file"
+    done
+}
+
+projects() {
+    # Clear existing content in the projects file
+    > "$projects_file"
+
+    projects=$(kubectl get appprojects -n $namespace --no-headers -o custom-columns=":metadata.name")
+
+    for project in $projects; do
+        kubectl get appprojects $project -n $namespace -o yaml | kubectl neat >> "$projects_file"
+        echo "---" >> "$projects_file"
+    done
+}
+
+# Check the command-line arguments
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Usage: ./my_script.sh <name>"
+    exit 1
+fi
+
+function_name=$1
+
+case $function_name in
+        apps)
+        apps
+        ;;
+        projects)
+        projects
+        ;;
+    *)
+        echo "Invalid function name. Available functions: apps, projects"
+        exit 1
+        ;;
+esac
